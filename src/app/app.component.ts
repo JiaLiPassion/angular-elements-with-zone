2ca21cce242024e5dce327ab434bff7042e9f8ac
JiaLiPassion <JiaLi.Passion@gmail.com>
2018-04-30 11:58:01 +0800

    update sample with output event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-elem',
@@ -7,10 +7,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   @Input() title: string;
+  @Output() update = new EventEmitter();
 
+  count: number = 0;
   zone: string;
 
   ngOnInit() {
     this.zone = (window as any).Zone.current.name;
+  }
+
+  onClick() {
+    this.title = 'updated' + this.count++ + this.title;
+    this.update.emit(this.title);
   }
 }
